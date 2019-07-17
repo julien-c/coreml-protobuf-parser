15d05e4a866714abf8fc1a588533c3fd1511fd82
Julien Chaumond <chaumond@gmail.com>
2019-07-16 20:15:26 -0400

    Generate static code from .proto files
    cc @lysandrejik

---
+++ parse.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs';
+import { c } from './lib/Log';
+import { CoreML } from './mlmodel';
+
+/**
+ * Generate static code from .proto files using:
+ * 
+ * ./node_modules/.bin/pbjs -t static-module -w commonjs --es6 -o mlmodel.js mlmodel/format/Model.proto
+ * ./node_modules/.bin/pbts -o mlmodel.d.ts mlmodel.js
+ */
+
+
+(async () => {
+	/**
+	 * Get "spec" of a model.
+	 */
+	const buf = await fs.promises.readFile(
+		// './examples/SentimentPolarity.mlmodel'
+		process.argv[process.argv.length - 1]
+	);
+	const m = CoreML.Specification.Model.decode(buf);
+	
+	c.debug(m.description);
+	c.log(`===`);
+	c.log(m);
+})();
