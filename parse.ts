6399a13248039253eb9d8569fee82f930862e69b
Julien Chaumond <chaumond@gmail.com>
2019-07-19 16:35:27 -0400

    gpt2 layers from onnx export
    Co-Authored-By: Lysandre Debut <lysandre.debut@reseau.eseo.fr>

--- parse.ts
+++ parse.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import { c } from './lib/Log';
 import { CoreML } from './mlmodel';
+import { onnx } from './onnx-proto';
 import { inspect } from 'util';
 
 /**
@@ -8,31 +9,46 @@ import { inspect } from 'util';
  * 
  * ./node_modules/.bin/pbjs -t static-module -w commonjs --es6 -o mlmodel.js mlmodel/format/Model.proto
  * ./node_modules/.bin/pbts -o mlmodel.d.ts mlmodel.js
+ * 
+ * onnx
+ * ./node_modules/.bin/pbjs -t static-module -w commonjs --es6 -o onnx-proto.js onnx/onnx.proto onnx/onnx-operators.proto
+ * ./node_modules/.bin/pbts -o onnx-proto.d.ts onnx-proto.js
+ * 
  */
 
+ const MLMODEL = false;
 
 (async () => {
-	/**
-	 * Get "spec" of a model.
-	 */
+	if (MLMODEL) {
+		/**
+		 * Get "spec" of a model.
+		 */
+		const buf = await fs.promises.readFile(
+			// './examples/SentimentPolarity.mlmodel'
+			process.argv[process.argv.length - 1]
+		);
+		const m = CoreML.Specification.Model.decode(buf);
+		
+		// c.debug(m.description);
+		// c.log(`===`);
+		console.log(
+			inspect(m, {
+				depth: Infinity,
+				maxArrayLength: Infinity,
+			})
+		);
+	}
+	
 	const buf = await fs.promises.readFile(
-		// './examples/SentimentPolarity.mlmodel'
 		process.argv[process.argv.length - 1]
 	);
-	const m = CoreML.Specification.Model.decode(buf);
+	const m = onnx.ModelProto.decode(buf);
 	
-	// c.debug(m.description);
-	// c.log(`===`);
 	console.log(
 		inspect(m, {
+			colors: true,
 			depth: Infinity,
 			maxArrayLength: Infinity,
 		})
 	);
-	c.log(`===`);
-	
-	const layers = m.neuralNetwork!.layers! as CoreML.Specification.NeuralNetworkLayer[];
-	for (const layer of layers) {
-		c.log(layer.layer);
-	}
 })();
